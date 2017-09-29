20db0bc9ee49f3f8d5c0ee5a6480b1517d4b36d9
eof <eof000@qq.com>
2017-09-29 18:03:06 +0800

    log4j

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp target/classes/com /usr/local/tomcat/webapps/ROOT/WEB-INF/classes -rf
-cp log4j.properties /usr/local/tomcat/webapps/ROOT/WEB-INF/config -rf
+cp log4j.properties /usr/local/tomcat/webapps/ROOT/WEB-INF/config/log4j.properties -rf
