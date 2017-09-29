fb14fb9248b39a3b3732766fa80eeb1105005c16
eof <eof000@qq.com>
2017-09-29 10:54:35 +0800

    add libs

--- copy_libs.sh
+++ copy_libs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp ~/.m2/repository/org/codehaus/jettison/jettison/1.3.7/jettison-1.3.7.jar /usr/local/tomcat/webapps/ROOT/WEB-INF/lib -rf
+cp ~/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar /usr/local/tomcat/webapps/ROOT/WEB-INF/lib -rf
